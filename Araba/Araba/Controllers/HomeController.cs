0411e5b8cb187d74fc8249c975b34ea24982277d
Yasir Kaş <yasirkas77@hotmail.com>
2023-11-19 15:58:26 +0300

    İlan detay sayfası dinamik hale getirildi.

--- Araba/Araba/Controllers/HomeController.cs
+++ Araba/Araba/Controllers/HomeController.cs
@@ -20,9 +20,12 @@ namespace Araba.Controllers
             var ilan = db.Ilans.Include(m => m.Model).ToList();
             return View(ilan);
         }
-        public ActionResult Detay()
+        public ActionResult Detay(int id)
         {
-            return View();
+            var ilan = db.Ilans.Where(i => i.IlanId == id).Include(m => m.Model).Include(m => m.Durum).Include(m => m.Sehir).Include(m => m.Model).FirstOrDefault();
+            var imgs = db.Resims.Where(i => i.IlanId == id).ToList();
+            ViewBag.imgs = imgs;
+            return View(ilan);
         }
     }
 }
