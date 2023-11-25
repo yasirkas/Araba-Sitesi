bffde05eeb28f8017a37f8745f4d8aeeb7e03474
Yasir Kaş <yasirkas77@hotmail.com>
2023-11-25 11:42:03 +0300

    +

--- Araba/Araba/Controllers/HomeController.cs
+++ Araba/Araba/Controllers/HomeController.cs
@@ -27,7 +27,7 @@ namespace Araba.Controllers
             var ara = db.Ilans.Include(m => m.Model);
             if (!String.IsNullOrEmpty(q))
             {
-                ara = ara.Where(i => i.Aciklama.Contains(q) || i.Model.ModelAd.Contains(q));
+                ara = ara.Where(i => i.Aciklama.Contains(q) || i.Model.ModelAd.Contains(q) || i.Model.Marka.MarkaAd.ToLower().Contains(q));
             }
             return View(ara.ToList());
         }
