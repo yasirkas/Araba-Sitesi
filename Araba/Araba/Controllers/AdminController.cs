997994ba67007f9c38a7e50a98e8589a2905881e
Yasir Kaş <yasirkas77@hotmail.com>
2023-11-26 08:57:30 +0300

    Admin Paneli aktif edildi
    Kullanıcıların yetkisi olmayan yerlere girmesi engellendi.
    Panel düzenlendi.

---
+++ Araba/Araba/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Araba.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace Araba.Controllers
+{
+    [Authorize(Roles ="admin")]
+    public class AdminController : Controller
+    {
+        DataContext db = new DataContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult IlanListesi()
+        {
+            var ilan = db.Ilans.Include(i => i.Model).Include(i => i.Durum).Include(i => i.Sehir).ToList();
+            return View(ilan);
+        }
+    }
+}
